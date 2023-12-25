88926a942c108eeaaaa266abe6f2464c05df0d7f
MyroslavaRogoza <miroslavarogozayyy@gmail.com>
2023-12-25 22:10:01 +0200

    HW-06

--- js/task-3.js
+++ js/task-3.js
@@ -7,15 +7,15 @@ class StringBuilder{
         return this.#value;
     } 
     padStart(str){
-         this.#value= this.#value.padStart(2,str); 
+         this.#value = this.#value.padStart(2,str); 
          return this.#value;
     }
     padEnd(str){
-          this.#value=this.#value.padEnd(3,str);
+          this.#value = this.#value.padEnd(3,str);
           return this.#value;    
     }
     padBoth(str){
-        this.#value=this.#value.padStart(4,str).padEnd(5,str);
+        this.#value = this.#value.padStart(4,str).padEnd(5,str);
        return this.#value;
     }
 }
